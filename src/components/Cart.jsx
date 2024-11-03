88d851501b7bdf96b38faa39848c188e90d5571b
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-11-03 16:30:25 +0200

    feat: add cart style

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,14 +5,21 @@ import { Card } from 'react-bootstrap';
 export default function Cart() {
     const cartItems = useSelector((state) => state.cart.cart); // get cart items from redux
 
-    // Calculate total price
-    const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+    let totalPrice = 0;
+
+    for (let i = 0; i < cartItems.length; i++) {
+        // Loop through each item in the cartItems array
+        totalPrice += cartItems[i].price; // Add the price of each item to totalPrice
+    }
+
+    // Round totalPrice to nearest two decimals
+    totalPrice = Math.round(totalPrice * 100) / 100;
 
     return (
         <div className="cart text-light">
-            <h2>Shopping Cart</h2> {/* cart header */}
+            <h2>Shopping Cart</h2>
             {cartItems.length === 0 ? (
-                <p>No items in cart</p> // show if cart is empty
+                <p>No items in cart</p>
             ) : (
                 <>
                     {cartItems.map((item, index) => (
@@ -22,8 +29,8 @@ export default function Cart() {
                             style={{ marginBottom: '10px', backgroundColor: '#444' }}
                         >
                             <Card.Body>
-                                <Card.Title>{item.title}</Card.Title> {/* title */}
-                                <Card.Text>Price: R{item.price}</Card.Text> {/* price */}
+                                <Card.Title>{item.title}</Card.Title>
+                                <Card.Text>Price: R{item.price}</Card.Text>
                             </Card.Body>
                         </Card>
                     ))}
@@ -31,7 +38,6 @@ export default function Cart() {
                     {totalPrice > 0 && (
                         <div className="total-price" style={{ marginTop: '20px', color: '#fff' }}>
                             {' '}
-                            {/* !!CHANGE!! add total price display inside cart */}
                             <h2>Total Price: R{totalPrice}</h2>
                         </div>
                     )}
